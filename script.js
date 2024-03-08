91c6f755b1f7c672842ee0b806332d743101b4fb
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 22:33:41 +0530

    Finish

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+const button = document.querySelectorAll('.show-modal');
+const toggleEvent = function () {
+  document.querySelector('.modal').classList.toggle('hidden');
+  document.querySelector('.overlay').classList.toggle('hidden');
+};
+const clickEvent = function () {
+  toggleEvent();
+};
+for (let i = 0; i < button.length; i += 1) {
+  button[i].addEventListener('click', clickEvent);
+}
+document.querySelector('.close-modal').addEventListener('click', function () {
+  toggleEvent();
+});
+document.querySelector('.overlay').addEventListener('click', toggleEvent);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    toggleEvent();
+  }
+});
